f7809bc058ad14726c86af8e2c4c2a07fb89b383
kenjiuno <h8mastre@gmail.com>
2015-12-19 11:29:32 +0900

    0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ClipPaste {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(3);
+            this.label1.Size = new System.Drawing.Size(284, 61);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(ここに画像・写真ファイルをドロップ)";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(284, 61);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Clip Paste";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ClipPaste {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            var rc = Screen.GetWorkingArea(this);
+            Left = rc.Right - Width;
+            Top = rc.Bottom - Height;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e) {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        String fpPNG;
+
+        private void Form1_DragDrop(object sender, DragEventArgs e) {
+            label1.Hide();
+
+            String s = "" + e.Data.GetData("Sender");
+            if (s == "" + GetHashCode()) return;
+
+            var alfp = e.Data.GetData(DataFormats.FileDrop) as String[];
+            if (alfp != null) {
+                foreach (var fp in alfp) {
+                    try {
+                        Bitmap pic = new Bitmap(fp);
+                        float nw = pic.Width / pic.HorizontalResolution * 96;
+                        float nh = pic.Height / pic.VerticalResolution * 96;
+                        var rc = Screen.GetWorkingArea(this);
+                        rc.Inflate(-rc.Width / 5, -rc.Height / 5);
+                        float f = Math.Max(1, Math.Max(nw / rc.Width, nh / rc.Height));
+
+                        Debug.WriteLine("# {0},{1}  {2},{3}  {4}", pic.Width, pic.Height, pic.HorizontalResolution, pic.VerticalResolution, f);
+
+                        pic.SetResolution(pic.HorizontalResolution * f, pic.VerticalResolution * f);
+
+                        pic.Save(fpPNG = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N") + ".png"), ImageFormat.Png);
+                        BackgroundImage = pic;
+                    }
+                    catch (Exception err) {
+                        BackgroundImage = null;
+                        label1.Text = "" + err;
+                        label1.Show();
+                    }
+                    break;
+                }
+            }
+            else {
+            }
+        }
+
+        DataObject ole = new DataObject();
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e) {
+            if (BackgroundImage == null) return;
+            ole.SetImage(BackgroundImage);
+            ole.SetData(DataFormats.FileDrop, new String[] { fpPNG });
+            ole.SetData("Sender", GetHashCode());
+            DoDragDrop(ole, DragDropEffects.All);
+        }
+    }
+}
